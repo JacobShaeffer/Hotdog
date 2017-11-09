369d3044047ad5c2a0b91954ea9b8b5c4284b0c8
Jacob Shaeffer <shaeffer.jacob@gmail.com>
2017-11-08 23:31:20 -0700

    25 cubes on the first plane, individual cube rotation

--- WorkBook/source/webgl.js
+++ WorkBook/source/webgl.js
@@ -2,25 +2,31 @@
 var canvas;
 var gl;
 
+var origin = mat4();
+var transformMatrix = new Float32Array([
+    1.0, 0.0, 0.0, 1.0,
+    0.0, 1.0, 0.0, 1.0,
+    0.0, 0.0, 1.0, 1.0,
+    0.0, 0.0, 0.0, 1.0  
+]);
+
 var NumVertices  = 36;
 
 var points = [];
 var colors = [];
 
-var xAxis = 0;
-var yAxis = 1;
-var zAxis = 2;
+var xRotation = 0;
+var yRotation = 0;
+var rotation = [ 0, 0, 0 ];
 
-var axis = 0;
-var theta = [ 0, 0, 0 ];
-
-var thetaLoc;
+var rotationLoc;
 
 // HW470: these variables are to allow the cubes to be scaled and translated
 var scaleLoc;
 //var scale = 0.5;
-var scale = [];
-var translate = [];
+var cubes = [];
+//var scale = [];
+//var translate = [];
 var numCubes = 0;
 
 var fovy = 60.0;
@@ -29,7 +35,10 @@ var near = 1.0;
 var far = 500.0;
 
 
-var eye = vec3(1.0, 0.0, 1.0);
+var radius = 1;
+var theta = 0;
+var phi = 0;
+var eye;// = vec3(0.0, 0.0, 1.0);
 var at = vec3(0.0, 0.0, 0.0);
 var up = vec3(0.0, 1.0, 0.0);
 
@@ -49,10 +58,8 @@ window.onload = function init()
     
     gl.enable(gl.DEPTH_TEST);
 
-    //
-    //  Load shaders and initialize attribute buffers
-    //
-    program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    //Load shaders and initialize attribute buffers
+    program = createProgram( gl );
     gl.useProgram( program );
     
     var cBuffer = gl.createBuffer();
@@ -63,12 +70,10 @@ window.onload = function init()
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
 	
-	// HW470: associate the locale variable scaleLoc with the shader variable scale
     scaleLoc = gl.getUniformLocation(program, 'scale');
     
-    //addCube(128, 128);
-    addCube(256, 256);
-    //addCube(384, 384);
+    for(var i=0; i<25; i++)
+        addCube(i);
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
@@ -77,40 +82,42 @@ window.onload = function init()
     var vPosition = gl.getAttribLocation( program, "vPosition" );
     gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vPosition );
-
-    thetaLoc = gl.getUniformLocation(program, "theta"); 
     
-    //event listeners for buttons, slider, and canvas
-	
-	// HW470: when the canvas is clicked get the location and place a new cube centered there
+    rotationLoc = gl.getUniformLocation(program, "rotation"); 
+    
 	canvas.onclick = function(e){
 				
 		//add new cube with above translation
     };
     
-    document.getElementById( "xButton" ).onclick = function () {
-        axis = xAxis;
-    };
-    document.getElementById( "yButton" ).onclick = function () {
-        axis = yAxis;
-    };
-    document.getElementById( "zButton" ).onclick = function () {
-        axis = zAxis;
-    };
+    document.getElementById( "up"    ).onmousedown = () => { xRotation = -1; };
+    document.getElementById( "up"    ).onmouseup   = () => { xRotation =  0; };
+    document.getElementById( "down"  ).onmousedown = () => { xRotation =  1; };
+    document.getElementById( "down"  ).onmouseup   = () => { xRotation =  0; };
+    document.getElementById( "right" ).onmousedown = () => { yRotation =  1; };
+    document.getElementById( "right" ).onmouseup   = () => { yRotation =  0; };
+    document.getElementById( "left"  ).onmousedown = () => { yRotation = -1; };
+    document.getElementById( "left"  ).onmouseup   = () => { yRotation =  0; };
         
     render();
 }
 
-function addCube(originX, originY){
-    var rect = canvas.getBoundingClientRect();
-    var x = originX - rect.left;
-    var y = originY - rect.top;
-    x = 0;//(x/512 * 2) - 1;
-    y = 0;//1 - (y/512 * 2);
-    var z = -1;
-    translate.push([x, y, z]);
-    scale.push(1);
+function addCube(number){
+    var x = (number % 5.0) * 2.0;
+    var z = (- Math.floor(number / 5.0) - 3.0) * 2.0;
+    var y = -4.0 * 0.25;
+    x -= 4.0;
+    x = x*0.25;
+    z = z*0.25;
     numCubes++;
+
+    cubes.push(
+        {
+            translate: {x:0.0, y:0.0, z:0.0},
+            origin: {x:x, y:y, z:z},
+            scale: 0.25,
+        }
+    );
 }
 
 function colorCube()
@@ -140,40 +147,31 @@ function quad(a, b, c, d)
         [ 0.0, 0.0, 0.0, 1.0 ],  // black
         [ 1.0, 0.0, 0.0, 1.0 ],  // red
         [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
-        [ 0.0, 1.0, 0.0, 1.0 ],  // green
-        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
-        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
-        [ 0.0, 1.0, 1.0, 1.0 ],  // white
-        [ 1.0, 1.0, 1.0, 1.0 ]   // cyan
+        [ 0.0, 1.0, 0.0, 1.0 ],  // orange (green)
+        [ 1.0, 0.5, 0.0, 1.0 ],  // blue (orange)
+        [ 1.0, 1.0, 1.0, 1.0 ],  // magenta (white)
+        [ 0.0, 0.0, 1.0, 1.0 ],  // white (blue)
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
     ];
-
-    // We need to parition the quad into two triangles in order for
-    // WebGL to be able to render it.  In this case, we create two
-    // triangles from the quad indices
-    
-    //vertex color assigned by the index of the vertex
     
     var indices = [ a, b, c, a, c, d ];
 
     for ( var i = 0; i < indices.length; ++i ) {
         points.push( vertices[indices[i]] );
         colors.push( vertexColors[a] );
-    
-        // for solid colored faces use 
-        //colors.push(vertexColors[a]);
-        
     }
 }
 
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-	
-    theta[axis] += 1.0;
-    gl.uniform3fv(thetaLoc, theta);
+    
+    rotation[0] += xRotation;
+    rotation[1] += yRotation;
+    gl.uniform3fv(rotationLoc, rotation);
 
+    eye = vec3(radius * Math.sin(theta) * Math.cos(phi), radius * Math.sin(theta) * Math.sin(phi), radius * Math.cos(theta));
 
-    //CSE470 calculate eye positioning and orthographic view box
     modelViewMatrix = lookAt(eye, at , up);
     projectionMatrix = perspective(fovy, aspect, near, far);
 
@@ -183,21 +181,51 @@ function render()
 	gl.uniformMatrix4fv(modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv(projectionMatrixLoc, false, flatten(projectionMatrix) );
     
-	
-	// HW470: create the matrix for scaling and translation then apply it through uniformMatrix4fv
 	for(var i=0; i<numCubes; i++)
 	{
-		var scaleMatrix = new Float32Array([
-			scale[i], 0.0,   0.0,   translate[i][0],
-			0.0,   scale[i], 0.0,   translate[i][1],
-			0.0,   0.0,   scale[i], translate[i][2],
-			0.0,   0.0,   0.0,   1.0  
-		]);
-		gl.uniformMatrix4fv(scaleLoc, false, scaleMatrix);
-		gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
-
+        renderCube(i);
 	}
 
     requestAnimFrame( render );
+}
+
+var a = 0;
+
+function renderCube(cubeID)
+{
+    var cubeOrigin = cubes[cubeID].origin;
+    var cubeTranslate = cubes[cubeID].translate;
+
+    /*
+    //cubes[cubeID].shpereicalOrigin.theta += yRotation * Math.PI / 180;
+    //cubes[cubeID].shpereicalOrigin.phi += xRotation * Math.PI / 180;
+    var radius = Math.sqrt(cubeOrigin.x*cubeOrigin.x + cubeOrigin.y*cubeOrigin.y + cubeOrigin.z*cubeOrigin.z);
+    var theta = Math.acos(cubeOrigin.z / radius);
+    var phi = Math.atan2(cubeOrigin.y, cubeOrigin.x);
+
+    var x = radius * Math.sin(theta) * Math.cos(phi);
+    var y = radius * Math.sin(theta) * Math.sin(phi);
+    var z = radius * Math.cos(theta);
+
+    if(a < 20){
+        console.log("shpereicalOrigin:      " + radius, theta, phi);
+        console.log("cubeOrigin:            " + JSON.stringify(cubeOrigin));
+        console.log("shpereicalToCartesian: " + JSON.stringify({x:x, y:y, z:z}));
+        a++;
+    }
+    */
+    
+    var scaling = cubes[cubeID].scale;
+    var scaleMatrix = new Float32Array([
+        scaling, 0.0,     0.0,     cubeOrigin.x + cubeTranslate.x,
+        0.0,     scaling, 0.0,     cubeOrigin.y + cubeTranslate.y,
+        0.0,     0.0,     scaling, cubeOrigin.z + cubeTranslate.z,
+        0.0,     0.0,     0.0,     1.0  
+    ]);
+
+    scaleMatrix = mult(scaleMatrix, transformMatrix);
+
+    gl.uniformMatrix4fv(scaleLoc, false, scaleMatrix);
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
 }
 
